92a2b41fa0029379871b5f99415a25b1fa8c12b6
Tejas Rupesh Naringrekar <87133442+legend100k@users.noreply.github.com>
2025-04-27 01:26:07 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,3 +1,41 @@
+//laplacian
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+def laplacian_sharpen(image):
+    laplacian_kernel = np.array([[0, -1, 0],
+
+                                 [-1, 4, -1],
+
+                                 [0, -1, 0]])
+
+    height, width = image.shape
+    sharpened_image = np.zeros((height, width), dtype=np.float32)
+    padded_image = np.pad(image, pad_width=1, mode='edge')
+    for i in range(height):
+        for j in range(width):
+            region = padded_image[i:i+3, j:j+3]
+            sharpened_value = np.sum(region * laplacian_kernel)
+            sharpened_image[i, j] = np.clip(image[i, j] + sharpened_value, 0, 255)
+    return sharpened_image.astype(np.uint8)
+image_path = 'path_to_your_image.jpg'  
+image = Image.open(image_path).convert('L')  
+image_array = np.array(image)
+sharpened_image = laplacian_sharpen(image_array)
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+plt.title('Original Image')
+plt.imshow(image_array, cmap='gray')
+plt.axis('off')
+
+plt.subplot(1, 2, 2)
+plt.title('Sharpened Image')
+plt.imshow(sharpened_image, cmap='gray')
+plt.axis('off')
+plt.show()
+
+
+
 //averaging filter
 
 from PIL import Image
